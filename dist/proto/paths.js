d8e821b960dfb42c931b3d63e1047cbf942c6f9a
scvrylullaby <vadimzooru@gmail.com>
2026-05-11 20:44:00 +0800

    feat: added the proto contracts in dist

--- dist/proto/paths.js
+++ dist/proto/paths.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PROTO_PATHS = void 0;
 const node_path_1 = require("node:path");
 exports.PROTO_PATHS = {
-    AUTH: (0, node_path_1.join)(__dirname, '../../proto/auth.proto'),
-    ACCOUNT: (0, node_path_1.join)(__dirname, '../../proto/account.proto'),
-    USER: (0, node_path_1.join)(__dirname, '../../proto/user.proto')
+    AUTH: (0, node_path_1.join)(__dirname, './contracts/auth.proto'),
+    ACCOUNT: (0, node_path_1.join)(__dirname, './contracts/account.proto'),
+    USER: (0, node_path_1.join)(__dirname, './contracts/user.proto')
 };
--- lib/proto/paths.ts
+++ lib/proto/paths.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 
 export const PROTO_PATHS = {
-  AUTH: join(__dirname, '../../proto/auth.proto'),
-  ACCOUNT: join(__dirname, '../../proto/account.proto'),
-  USER: join(__dirname, '../../proto/user.proto')
+  AUTH: join(__dirname, './contracts/auth.proto'),
+  ACCOUNT: join(__dirname, './contracts/account.proto'),
+  USER: join(__dirname, './contracts/user.proto')
 } as const
