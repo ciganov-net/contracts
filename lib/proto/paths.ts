a0b7bdaf1cbde22ce696a1ef957314955f50ab75
scvrylullaby <vadimzooru@gmail.com>
2026-05-19 18:54:27 +0800

    feat: add the balance proto contracts

--- lib/proto/paths.ts
+++ lib/proto/paths.ts
@@ -3,5 +3,6 @@ import { join } from 'node:path'
 export const PROTO_PATHS = {
   AUTH: join(__dirname, './contracts/auth.proto'),
   ACCOUNT: join(__dirname, './contracts/account.proto'),
-  USER: join(__dirname, './contracts/user.proto')
+  USER: join(__dirname, './contracts/user.proto'),
+  BALANCE: join(__dirname, './contracts/balance.proto')
 } as const
